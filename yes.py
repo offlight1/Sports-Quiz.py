68848973346d9f8f9ca6e81337b6333c892a17b6
offlight1 <100810134+offlight1@users.noreply.github.com>
2022-03-09 21:12:36 -0500

    Add files via upload
    This is the code for the quiz. Type everything in lowercase. This is a  simple code so I only added 2 notes on the code describing it. Use it for reference or replace the questions with your own.

---
+++ yes.py
@@ -0,0 +1,63 @@
+
+
+
+print("welcome to my sports quiz! PLEASE TYPE EVERYTHING IN LOWERCASE")      
+answer_yes = input('do you want to play?')
+#here we tell the system that if the answer to "do you want to play", is yes, to continue. If not the program will quit
+if answer_yes == 'yes':
+     print ("Okay, Lets Play!")
+else :
+    if answer_yes == 'no':
+        print('please close the program')
+        quit
+score = 0
+if answer_yes == 'yes':
+  print("lets play")
+answer = 'random'  
+   
+#we state that if answer_yes = 'yes', to show the questions. this is because when you first start this program, 
+# if you type yes the program continues. we repeat this every question
+if answer_yes == 'yes':
+  answer = input(" What team did Tom Brady play the most years for?")
+  if answer == 'patriots':
+    print("correct")
+    score = score + 1 
+else: 
+    if answer_yes == 'yes':
+     print("not correct")
+     score = score + 0
+
+if answer_yes == 'yes':
+ answer = input("Q:2, which nba player of old times is considered the best of all time?")
+if answer == 'micheal jordan':
+    print("correct")
+    score = score + 1
+else:
+    if answer_yes == 'yes':
+        print("not correct")  
+        score = score + 0
+    
+
+if answer_yes == 'yes':    
+ answer = input('What sport did Jackie Robinson play? ')
+if answer == 'baseball':
+    print("correct")
+    score = score + 1
+else:
+     if answer_yes == 'yes':
+      print("not correct")   
+      score = score + 0
+
+if answer_yes == 'yes':
+ answer = input("who is considered the best player of all time in the NFL?")
+if answer == 'tom brady':
+ print("correct")
+ score = score + 1
+else:
+    if answer_yes == 'yes':
+     print('not correct')
+     score = score + 0
+
+
+if answer_yes == 'yes':
+ print( " Welcome to the end of the quiz! You got", score, "points out of 4!" )
